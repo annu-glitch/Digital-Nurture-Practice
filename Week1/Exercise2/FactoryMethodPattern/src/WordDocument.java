b5fe9b4ad3dbd7ca51db0b26ced2dd395ebdabee
annu-glitch <2205448@kiit.ac.in>
2025-06-17 12:23:29 +0530

    Create WordDocument.java

---
+++ Week1/Exercise2/FactoryMethodPattern/src/WordDocument.java
@@ -0,0 +1,5 @@
+class WordDocument implements Document{   //concrete product class 1, implements product
+    public void open(){
+        System.out.println("Opening a Word Document...");
+    }
+}
