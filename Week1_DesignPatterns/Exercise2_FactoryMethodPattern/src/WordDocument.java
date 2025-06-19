61f13ff70019301d9502983395c9d1296d57f498
annu-glitch <2205448@kiit.ac.in>
2025-06-19 18:44:41 +0530

    Delete Week1_DesignPatterns/Exercise2_FactoryMethodPattern/src/WordDocument.java

--- Week1_DesignPatterns/Exercise2_FactoryMethodPattern/src/WordDocument.java
+++
@@ -1,5 +0,0 @@
-class WordDocument implements Document{   //concrete product class 1, implements product
-    public void open(){
-        System.out.println("Opening a Word Document...");
-    }
-}
