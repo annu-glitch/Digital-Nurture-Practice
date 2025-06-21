57629810b1b875fa2a6f71f3d9e513ece31263cf
Annu <2205448@kiit.ac.in>
2025-06-21 23:05:12 +0530

    Moved files out of Exercise1_WindowFunctions and deleted folder

--- Week2_AdvancedSQL_Nunit/Exercise1_WindowFunctions/codes/DENSE_RANK.sql
+++
@@ -1,5 +0,0 @@
---assigns same rank to tied values
---does not skip the number after tie
-select ProductID, ProductName, Category, price,
-DENSE_RANK() OVER (partition by Category order by price desc) as rank2
-from Product;
--- Week2_AdvancedSQL_Nunit/Exercise1_WindowFunctions/codes/ROW_NUMBER.sql
+++
@@ -1,5 +0,0 @@
---assigns unique ranks
-
-select ProductID, ProductName, Category, price,
-ROW_NUMBER() OVER (partition by Category order by price desc) as unique_rank
-from Product;
