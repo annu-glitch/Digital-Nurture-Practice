a5b6503ea95946956627f1c9bb01218caa4455aa
annu-glitch <2205448@kiit.ac.in>
2025-06-21 22:32:10 +0530

    Update ROW_NUMBER.sql

--- Week2/AdvancedSQL_&_NUnit/Exercise1_WindowFunctions/ROW_NUMBER.sql
+++ Week2/AdvancedSQL_&_NUnit/Exercise1_WindowFunctions/ROW_NUMBER.sql
@@ -1,5 +1,5 @@
 --ROW_NUMBER() to assign unique ranks within each category
 
 select ProductID, ProductName, Category, price,
-ROW_NUMBER() OVER (partition by Category order by price) as unique_rank
+ROW_NUMBER() OVER (partition by Category order by price desc) as unique_rank
 from Product;
