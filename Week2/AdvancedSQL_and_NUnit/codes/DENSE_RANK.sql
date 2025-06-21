9eb184a5aaee1d0c810f58fd78765c66981a205e
annu-glitch <2205448@kiit.ac.in>
2025-06-21 22:44:12 +0530

    Delete Week2 directory

--- Week2/AdvancedSQL_and_NUnit/codes/DENSE_RANK.sql
+++
@@ -1,7 +0,0 @@
---handling tied ranks
---gives same rank to tied values
---does not skip number after a tie
-
-select ProductID, ProductName, Category, price,
-DENSE_RANK() OVER (partition by Category order by price desc) as rank2
-from Product;
