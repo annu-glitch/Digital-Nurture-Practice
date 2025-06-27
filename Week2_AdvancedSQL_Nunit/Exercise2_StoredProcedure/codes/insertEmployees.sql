0c1c785ebc699a9f4676ced99faa7b595c33a812
annu-glitch <2205448@kiit.ac.in>
2025-06-27 16:39:07 +0530

    Create insertEmployees.sql

---
+++ Week2_AdvancedSQL_Nunit/Exercise2_StoredProcedure/codes/insertEmployees.sql
@@ -0,0 +1,16 @@
+--create a procedure to insert a new record in Employees table
+CREATE PROCEDURE sp_InsertEmployees
+	(@EmployeeID INT,
+	@FirstName VARCHAR(50),
+	@LastName VARCHAR(50),
+	@DepartmentID INT,
+	@Salary DECIMAL(10, 2),
+	@JoinDate DATE)
+AS
+BEGIN
+	INSERT INTO Employees(EmployeeID, FirstName, LastName, DepartmentID, Salary, JoinDate)
+	VALUES (@EmployeeID, @FirstName, @LastName, @DepartmentID, @Salary, @JoinDate);
+END;
+
+--execute procedure
+EXEC sp_InsertEmployees 5, 'Annu', 'Priya', '3', '78000.56', '2025-08-29';
