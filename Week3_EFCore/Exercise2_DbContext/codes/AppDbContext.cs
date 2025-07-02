2ee43cbe8c4de6357eef53759c5722e55c0192bd
annu-glitch <2205448@kiit.ac.in>
2025-07-02 09:54:17 +0530

    Create AppDbContext.cs

---
+++ Week3_EFCore/Exercise2_DbContext/codes/AppDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using RetailInventoryProject.Models;
+
+namespace RetailInventoryProject
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=RetailInventoryDB;Trusted_Connection=True;");
+            }
+        }
+    }
+}
