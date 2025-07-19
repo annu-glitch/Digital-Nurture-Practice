b38116509ab06210422b7a7e79329aa415f9a365
annu-glitch <2205448@kiit.ac.in>
2025-07-19 23:19:14 +0530

    Update LoginModel.cs

--- Week5_Microservices/Exercise2_JwtAuthentication/codes/LoginModel.cs
+++ Week5_Microservices/Exercise2_JwtAuthentication/codes/LoginModel.cs
@@ -1,6 +1,6 @@
-namespace JwtAuth.Models
+namespace JwtAuth.Models   
 {
-    public class LoginModel
+    public class LoginModel    //acting only as a data transfer object to receive API data, no real database connectin
     {
         public string Username { get; set; }
         public string Password { get; set; }
