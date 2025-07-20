5b2af7df886eb3f57147ef731c59512069acbdc4
annu-glitch <2205448@kiit.ac.in>
2025-07-20 20:21:09 +0530

    Delete Week5_Microservices/Exercise2_JwtAuthentication directory

--- Week5_Microservices/Exercise2_JwtAuthentication/codes/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-namespace JwtAuth.Models   
-{
-    public class LoginModel    //acting only as a data transfer object to receive API data, no real database connectin
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
