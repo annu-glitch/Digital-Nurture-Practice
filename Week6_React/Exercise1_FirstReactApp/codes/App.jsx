8f9393bbb23d7b6bd7da6753b441e63200617d77
annu-glitch <2205448@kiit.ac.in>
2025-07-27 10:25:07 +0530

    Create App.jsx

---
+++ Week6_React/Exercise1_FirstReactApp/codes/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";         //imports React library to use JSX
+function App(){      //App: a functional component
+  return(
+    <h1>Welcome to the first session of React</h1>          //the component renders this; it's JSX
+  )
+}
+
+export default App;    //exporting App componeny so other files can use it
