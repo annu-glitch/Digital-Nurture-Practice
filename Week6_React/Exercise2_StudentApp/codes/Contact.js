a22fa9a1d062920e0c7dcffbe40821be3f2e1a4d
annu-glitch <2205448@kiit.ac.in>
2025-07-27 11:43:51 +0530

    Create Contact.js

---
+++ Week6_React/Exercise2_StudentApp/codes/Contact.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class Contact extends Component{   //named export; extends base class Component
+    render(){ //method that returns what to display in UI
+        return( //returns JSX
+            <div>    
+                <h3>Welcome to the Contact Page of Student Management Portal</h3>
+            </div>
+        );
+    }
+}
